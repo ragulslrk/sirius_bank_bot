40655f6427378b32dcb1fe8a0e2af2dee4b6dab9
ragulslrk <slrk4444@gmail.com>
2022-12-01 12:34:30 +0530

    ff

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get('/',(req,res)=>{
     console.log('index in runnning')
     res.render('index')
 })
-app.listen(process.env.PORT||8080,()=>{
+app.listen(process.env.PORT||3232,()=>{
     console.log('listening  bank bot')})
